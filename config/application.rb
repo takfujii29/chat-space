cb0fc263a1d023262f7fdb458322cd9e4cc54b45
takfujii29 <takfujii29@icloud.com>
2019-10-28 19:53:48 +0900

    Initial commit

---
+++ config/application.rb
@@ -0,0 +1,18 @@
+require_relative 'boot'
+
+require 'rails/all'
+
+# Require the gems listed in Gemfile, including any gems
+# you've limited to :test, :development, or :production.
+Bundler.require(*Rails.groups)
+
+module ChatSpace
+  class Application < Rails::Application
+    config.generators do |g|     # Settings in config/environments/* take precedence over those specified here.
+      g.stylesheets false      # Application configuration should go into files in config/initializers
+      g.javascripts false      # -- all .rb files in that directory are automatically loaded.
+      g.helper false
+      g.test_framework false
+    end
+  end
+end
