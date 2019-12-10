8ce2762f67c7f085a5b4c72d3450d1ac4d8acaa8
takfujii29 <takfujii29@icloud.com>
2019-12-10 15:36:03 +0900

    add-ajax

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,3 +1,55 @@
 $(function(){
+  function buildHTML(message){
+    if (message.image) {
+      var html = `<div class="chatdate__name">
+                    ${message.user_id}
+                  </div>
+                  <div class="chatdate__time">
+                    ${message.created_at}
+                  </div>
+                  <p class="message__content">
+                    ${message.content}
+                  </p>
+                  <img class="message__image" src="${message.image}">`
+    } else {
+      var html = `<div class="chatdate__name">
+                    ${message.user_id}
+                  </div>
+                  <div class="chatdate__time">
+                    ${message.created_at}
+                  </div>
+                  <p class="message__content">
+                    ${message.content}
+                  </p>`
+    }
+    return html;
+  }
 
-});
+
+
+
+  $('#new_message').on ('submit', function(e) {
+    e.preventDefault()
+    var formData = new FormData(this);
+    var url = $(this).attr('action')
+    $.ajax({
+        url: url,
+        type: 'POST',
+        data: formData,  
+        dataType: 'json',
+        processData: false,
+        contentType: false
+    })
+    .done(function(message){
+      var html = buildHTML(message);
+      $('.chat').append(html)
+      $('.chat').animate({ scrollTop: $('.chat')[0].scrollHeight});
+      $('#new_message')[0].reset();
+      $('.form__submit').prop("disabled", false);
+    })
+    .fail(function(){
+      alert("メッセージ送信に失敗しました");
+    });
+
+  })
+})
---
+++ app/views/messages/create.json.jbuilder
@@ -0,0 +1,7 @@
+json.content @message.content
+json.image @message.image.url
+json.group_id @message.group_id
+json.user_id @message.user.name
+json.created_at @message.created_at
+json.updated_at @message.updated_at
+
